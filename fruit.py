a5928bd7379e0b58efbfd1c13f8c60bf593f1e04
Babybold333 <maraljingoo515@gmail.com>
2025-02-19 10:36:09 +0800

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -1,1 +1,10 @@
-z
+class Food:
+
+    dex __init__(self):
+
+       x = random.randit(0, GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+       y = random.randit(0, GAME_HEIGHT / SPACE_SIZE) - 1) *SPACE_SIZE
+
+       self.coordinates = [x, y]
+
+       canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
